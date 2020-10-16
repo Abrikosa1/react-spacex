8b3c64ceba3700d91c4ded6e7840caa5e625a83a
Денис Иванов <world4den@mail.ru>
2020-10-16 12:59:27 +0600

    features + footer api update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,66 @@ import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
 import Calendar from './components/Calendar/Calendar';
 import Details from './components/Details/Details';
+
+import FetchData from './service/FetchData';
+
 import './style.css';
 
-function App() {
-  return (
-    <> 
-      <Header />
-      <Main />
-      <Features />
-      <Footer />
-    </> ///React.Fragment
- 
-  );
+class App extends React.Component {
+
+  // constructor(){
+  //   super();
+  //   this.state = 
+  // }
+  
+  fetchData = new FetchData();
+
+  state = {
+    rocket : 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+    company: null,
+  }
+
+  componentDidMount() {
+    this.updateRocket();
+    this.updateCompany();
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket()
+    .then(data =>{
+      this.setState({ rockets: data.map(item => item.name)})
+      return data;
+    })
+    .then(data => data.find(item => item.name === this.state.rocket))
+    .then(rocketFeatures => this.setState({ rocketFeatures }));
+  }
+  
+
+
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket);
+  }
+
+  updateCompany = () => {
+    this.fetchData.getCompany()
+    .then(company => this.setState({ company }))
+  }
+
+
+  render() {
+    return (
+      <> 
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
+        <Main rocket={this.state.rocket} />
+        {this.state.rocketFeatures && <Features {...this.state.rocketFeatures} />}
+        {this.state.company && <Footer {...this.state.company.links} />}
+      </> ///React.Fragment
+   );
+  }
 }
 
 export default App;
