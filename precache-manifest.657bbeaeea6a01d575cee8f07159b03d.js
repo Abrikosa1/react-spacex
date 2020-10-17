c68057cbd610628b22a87c49c19ed978a0be6c74
Денис Иванов <world4den@mail.ru>
2020-10-18 01:43:28 +0600

    Updates

---
+++ precache-manifest.657bbeaeea6a01d575cee8f07159b03d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f0b11d38f8434e6f843c35b6a92f01",
+    "url": "/react-spacex/home/index.html"
+  },
+  {
+    "revision": "ede8d85c198a79efc333",
+    "url": "/react-spacex/home/static/css/main.af42644f.chunk.css"
+  },
+  {
+    "revision": "cf04f42932f682c3a82d",
+    "url": "/react-spacex/home/static/js/2.2af4a158.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/react-spacex/home/static/js/2.2af4a158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede8d85c198a79efc333",
+    "url": "/react-spacex/home/static/js/main.ce55bee5.chunk.js"
+  },
+  {
+    "revision": "c18dc349790eb017391a",
+    "url": "/react-spacex/home/static/js/runtime-main.0c3b7339.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/react-spacex/home/static/media/logo.d5ab31ce.svg"
+  }
+]);
